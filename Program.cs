0fc7fadaef865f26a415d77d9defaf5e2138187c
Felipe Francisco <fefarias97@gmail.com>
2021-11-27 14:37:11 -0300

    resolução exercício tabuada

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+ int inicio, fim;
+
+Console.Write("Início: ");
+inicio = Convert.ToInt32(Console.ReadLine());
+Console.Write("Fim: ");
+fim = Convert.ToInt32(Console.ReadLine());
+if(inicio<fim){
+        while(inicio<=fim){
+            Console.Write($"{inicio}.. ");
+            inicio += 1;
+        }
+}else{
+    Console.Write("Erro, o final é menor que o começo.");
+}
